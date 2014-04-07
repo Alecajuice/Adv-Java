b0eea4258bea9dd0347f8535904026d324104481
Alex Tarng <alecadad@gmail.com>
2014-04-07 13:10:13 -0700

    asdf

---
+++ Exercise 1.10/src/MyDate.java
@@ -0,0 +1,8 @@
+
+public class MyDate
+{
+	private int year, month, day;
+	private static String[] strMonths = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}; 
+	private static String[] strDays = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}; 
+	
+}
