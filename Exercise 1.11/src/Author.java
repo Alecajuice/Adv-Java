884e10d3185e3ac7f7e8ce3f634b075efb874a1a
Alex Tarng <alecadad@gmail.com>
2014-04-11 13:13:49 -0700

    adsf

--- Exercise 1.11/src/Author.java
+++ Exercise 1.11/src/Author.java
@@ -1,31 +1,28 @@
 /*
  * Name: Alex Tarng
  * Course: Advanced Java
- * Date: 3/5/14
- * Assignment: Exercise 1.2
+ * Date: 4/11/14
+ * Assignment: Exercise 1.11
  * 
- * 		The Assignment begins by instructing us to create the Author class below with three instance variables (name:String, email:String, gender:char),
- * 		one constructor to initialize all of them, getName(), getEmail(), setEmail(), and getGender() getters and setters, and a toString() method
- * 		that returns "author-name (gender) at email".
- * 
- * 		Then the text tells us to create a TestAuthor class to test the constructor and public methods.
- * 
- * 		The assignment then instructs us to create the Book class with four instance variables (name:String, author:Author, price:double, and qtyInStock:int),
- * 		two constructors (one with and one without a default qtyInStock), getName(), getAuthor(), getPrice(), setPrice(), getQtyInStock(), and setQtyInStock()
- * 		getters and setters, and a toString() that returns "'book-name' by author-name (gender) at email".
- * 
- * 		The assignment asks us to create a TestBook class to test the constructor and public methods in the class Book.
- * 
- * 		The assignment then presents us with two Try's:
- * 
- * 		Try 1: Printing the name and email of the author from a Book instance. (Hint: aBook.getAuthor().getName(), aBook.getAuthor().getEmail()).
- * 			
- * 			So we implemented the print statement into the TestBook class.
- * 
- * 		Try 2: Introduce new methods called getAuthorName(), getAuthorEmail(), getAuthorGender() in the Book class to return the name, email and gender of the author of the book. For example,
- * 			public String getAuthorName() { ...... }
- * 			
- * 			So we implemented the new methods and tested them out in TestBook.
+ * 		The Assignment begins by instructing us to modify the Book class to support one or more authors by changing the instance variable authors to an Author array. Reuse the Author class written earlier.
+ * 			Notes:
+ * 				The constructors take an array of Author (i.e., Author[]), instead of an Author instance.
+ * 				The toString() method shall return "book-name by n authors", where n is the number of authors.
+ * 				A new method printAuthors() to print the names of all the authors.
+ * 			You are required to:
+ * 				Write the code for the Book class. You shall re-use the Author class written earlier.
+ *				 Write a test program (called TestBook) to test the Book class.
+ * 				Hints:
+ * 					// Declare and allocate an array of Authors
+ * 					Author[] authors = new Author[2];
+ * 					authors[0] = new Author("Tan Ah Teck", "AhTeck@somewhere.com", 'm');
+ * 					authors[1] = new Author("Paul Tan", "Paul@nowhere.com", 'm');
+ *  
+ * 					// Declare and allocate a Book instance
+ *   				Book javaDummy = new Book("Java for Dummy", authors, 19.99, 99);
+ *   				System.out.println(javaDummy);  // toString()
+ *   				System.out.print("The authors are: ");
+ *   				javaDummy.printAuthors();
  * 
  * 
  */
